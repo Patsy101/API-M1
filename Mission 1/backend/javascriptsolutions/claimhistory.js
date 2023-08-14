9649241bb1ef3bb1a65ca6fff097c3beea56aa57
Patrick tricenio <100541934+Patsy101@users.noreply.github.com>
2023-08-14 20:07:24 +1200

    Add files via upload
    to install modules click 
    
    1.  type npm install on terminal make sure it is on right folder directory
    2. start server by going to terminal by typing npm nodemon index.js
    3. start testing by going to terminal with the right folder directory using and typing npm test

---
+++ Mission 1/backend/javascriptsolutions/claimhistory.js
@@ -0,0 +1,97 @@
+// for https://www.nzta.govt.nz/roads-and-rail/rail/operating-a-railway/risk-management/risk-matrix-likelihood-and-consequence-tool/
+// breaking the questions down to smaller tasks
+function getRiskDescription(riskLevel) {
+  switch (riskLevel) {
+    case 1:
+      return 'Low risk driver';
+    case 2:
+      return 'Minor risk driver';
+    case 3:
+      return 'Moderate risk driver';
+    case 4:
+      return 'Moderate high risk driver';
+    case 5:
+      return 'High risk driver';
+    default:
+      return 'there is an error';
+  }
+}
+
+const riskLevel = '';
+const riskDescription = getRiskDescription(riskLevel);
+console.log(riskDescription);
+
+// //task 2
+// function getRiskInfo(description) {
+//   const keywordToRiskMap = {
+//     collide: { rating: 4, description: 'Moderate high risk driver' },
+//     scratch: { rating: 1, description: 'Low risk driver' },
+//     bump: { rating: 2, description: 'Minor risk driver' },
+//     crash: { rating: 5, description: 'High risk driver' },
+//     smash: { rating: 3, description: 'Moderate risk driver' },
+//   };
+
+//   const riskInfo = keywordToRiskMap[description.toLowerCase()];
+
+//   if (riskInfo !== undefined) {
+//     return riskInfo;
+//   } else {
+//     return 'there is an error';
+//   }
+// }
+
+// const description = 'collide';
+// const riskInfo = getRiskInfo(description);
+
+// if (typeof riskInfo === 'string') {
+//   console.log(riskInfo);
+// } else {
+//   console.log('Risk Rating:', riskInfo.rating);
+//   console.log('Risk Description:', riskInfo.description);
+// }
+
+//task 3
+
+function getRiskInfo(description) {
+  const keywordToRiskMap = {
+    collide: { rating: 4, description: 'Moderate high risk driver' },
+    scratch: { rating: 1, description: 'Low risk driver' },
+    bump: { rating: 2, description: 'Minor risk driver' },
+    crash: { rating: 5, description: 'High risk driver' },
+    smash: { rating: 3, description: 'Moderate risk driver' },
+  };
+
+  const words = description.toLowerCase().split(/\W+/);
+
+  let totalWeightedRating = 0;
+  let totalWeight = 0;
+
+  words.forEach((word, index) => {
+    if (keywordToRiskMap[word]) {
+      const weight = 1 / Math.abs(index - words.length / 2 + 0.5);
+      totalWeightedRating += keywordToRiskMap[word].rating * weight;
+      totalWeight += weight;
+    }
+  });
+
+  if (totalWeight === 0) {
+    return 'No relevant keywords found in description';
+  }
+
+  const averageWeightedRating = totalWeightedRating / totalWeight;
+
+  let closestRating = Math.round(averageWeightedRating);
+  closestRating = Math.min(Math.max(1, closestRating), 5);
+
+  return {
+    rating: closestRating,
+    description:
+      keywordToRiskMap[Object.keys(keywordToRiskMap)[closestRating - 1]]
+        .description,
+  };
+}
+
+const description = 'My car got collide and crash';
+const riskInfo = getRiskInfo(description);
+
+console.log('Risk Rating:', riskInfo.rating);
